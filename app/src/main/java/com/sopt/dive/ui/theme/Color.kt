78f086b2d260ead86c361cf558a1bee5bfb89224
dodo <eg4rang@naver.com>
2025-10-16 18:04:30 +0900

    [1차 필수] feat: theme 설정

--- app/src/main/java/com/sopt/dive/ui/theme/Color.kt
+++ app/src/main/java/com/sopt/dive/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Coral = Color(0xFFFB6F89)
+
+val Black1 = Color(0xFF222222)
+val Black2 = Color(0xFF5A5A5A)
+val Black3 = Color(0xFF9A9A9A)
+
+val Background = Color(0xFFFDFDFD)
