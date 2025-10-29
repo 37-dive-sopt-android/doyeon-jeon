5ad244adb0dab08857d10d215bca9ab2cc231a25
dodo <eg4rang@naver.com>
2025-10-30 01:26:46 +0900

    [2차 필수] feat: HomeScreen 구현 및Profile 목업 데이터 추가

--- app/src/main/java/com/sopt/dive/data/ProfileData.kt
+++ app/src/main/java/com/sopt/dive/data/ProfileData.kt
@@ -7,3 +7,66 @@ val myProfile: Profile = Profile(
     name = "도도",
     bio = "도도가 뛰면 도도도도",
 )
+
+val homeProfiles: List<Profile> = listOf(
+    Profile(
+        image = R.drawable.chopper,
+        name = "고고",
+        bio = "졸리당"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "노노",
+        bio = "잘거야잘거야잘거야!!!"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "로로로",
+        bio = "나 잘거야 말리지마!!!!!!!"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "모모",
+        bio = "하하이하이하이하이하이하이하이하이하이하이하이하이이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+    Profile(
+        image = R.drawable.chopper,
+        name = "보보",
+        bio = "안녕안녕안녕안녕안녕안녕"
+    ),
+)
--- app/src/main/java/com/sopt/dive/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/dive/ui/home/HomeScreen.kt
@@ -1,26 +1,48 @@
 package com.sopt.dive.ui.home
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.sopt.dive.R
+import com.sopt.dive.data.homeProfiles
 import com.sopt.dive.data.myProfile
 import com.sopt.dive.ui.component.ProfileCard
+import com.sopt.dive.ui.theme.Black3
 
 @Composable
 fun HomeScreen() {
-    LazyColumn(
+    Column(
         modifier = Modifier.padding(
             horizontal = 20.dp,
             vertical = 24.dp
         )
     ) {
-        item {
-            ProfileCard(
-                profile = myProfile
-            )
+        ProfileCard(
+            profile = myProfile
+        )
+        HorizontalDivider(
+            thickness = 2.dp,
+            color = Black3.copy(alpha = 0.6f),
+            modifier = Modifier.padding(top = 20.dp)
+        )
+        LazyColumn {
+            items(homeProfiles) {
+                ProfileCard(
+                    profile = it,
+                    modifier = Modifier.padding(top = 20.dp)
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
