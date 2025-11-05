4c242d301a6ae49efc0d7e226c27b7eb6ad170f6
dodo <eg4rang@naver.com>
2025-11-06 01:10:24 +0900

    [3차 필수] feat: 카드 뒤집기 구현

--- app/src/main/java/com/sopt/dive/ui/search/SearchScreen.kt
+++ app/src/main/java/com/sopt/dive/ui/search/SearchScreen.kt
@@ -1,8 +1,22 @@
 package com.sopt.dive.ui.search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.dive.R
+import com.sopt.dive.ui.component.DiveButton
+import com.sopt.dive.ui.component.FlippableCard
+import com.sopt.dive.ui.theme.Black2
 import com.sopt.dive.ui.theme.Typography
 
 @Composable
@@ -18,5 +32,32 @@ fun SearchRoute(
 fun SearchScreen(
     modifier: Modifier = Modifier,
 ) {
-    Text("검색", style = Typography.titleLarge, modifier = modifier)
+    var isFlipping by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "여기에 만드는 거 맞나?",
+            style = Typography.bodySmall,
+            color = Black2
+        )
+        FlippableCard(
+            isFlipping = isFlipping
+        )
+        DiveButton(
+            content = if (isFlipping) stringResource(R.string.card_end_button) else stringResource(R.string.card_start_button),
+            onClick = { isFlipping = !isFlipping },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+    SearchScreen(
+        modifier = Modifier.fillMaxSize()
+    )
 }
