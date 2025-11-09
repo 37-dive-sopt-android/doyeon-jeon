d183562d3d3d890911663e75670551b9d7d29a8c
dodo <eg4rang@naver.com>
2025-11-09 19:59:49 +0900

    [3차 심화] feat: DataStoreDataSource 구현

---
+++ app/src/main/java/com/sopt/dive/data/datasource/local/DataStoreDataSource.kt
@@ -0,0 +1,11 @@
+package com.sopt.dive.data.datasource.local
+
+import com.sopt.dive.data.model.AccountInfo
+import com.sopt.dive.data.model.UserPrefs
+
+interface DataStoreDataSource {
+    suspend fun getUserInfo(): UserPrefs?
+    suspend fun getAccountInfo(): AccountInfo?
+    suspend fun setUserInfo(id: String, pw: String, nickname: String, mbti: String)
+    suspend fun setLoginStatus(status: Boolean)
+}
---
+++ app/src/main/java/com/sopt/dive/data/datasource/local/DataStoreDataSourceImpl.kt
@@ -0,0 +1,55 @@
+package com.sopt.dive.data.datasource.local
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import com.sopt.dive.data.model.AccountInfo
+import com.sopt.dive.data.model.UserPrefs
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user")
+
+class DataStoreDataSourceImpl(
+    private val dataStore: DataStore<Preferences>,
+) : DataStoreDataSource {
+    override suspend fun getUserInfo(): UserPrefs? = dataStore.data.map { preferences ->
+        UserPrefs(
+            isLoggedIn = preferences[DataStoreKeys.IS_LOGGED_IN],
+            id = preferences[DataStoreKeys.ID],
+            pw = preferences[DataStoreKeys.PW],
+            nickname = preferences[DataStoreKeys.NICKNAME],
+            mbti = preferences[DataStoreKeys.MBTI]
+        )
+    }.firstOrNull()
+
+    override suspend fun getAccountInfo(): AccountInfo? = dataStore.data.map { preferences ->
+        AccountInfo(
+            id = preferences[DataStoreKeys.ID],
+            pw = preferences[DataStoreKeys.PW],
+        )
+    }.firstOrNull()
+
+    override suspend fun setUserInfo(
+        id: String,
+        pw: String,
+        name: String,
+        mbti: String,
+    ) {
+        dataStore.edit { user ->
+            user[DataStoreKeys.IS_LOGGED_IN] = false
+            user[DataStoreKeys.ID] = id
+            user[DataStoreKeys.PW] = pw
+            user[DataStoreKeys.NICKNAME] = name
+            user[DataStoreKeys.MBTI] = mbti
+        }
+    }
+
+    override suspend fun setLoginStatus(status: Boolean) {
+        dataStore.edit { user ->
+            user[DataStoreKeys.IS_LOGGED_IN] = true
+        }
+    }
+}
