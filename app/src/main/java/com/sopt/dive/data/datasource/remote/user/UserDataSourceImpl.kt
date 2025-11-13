275e7ec70290b56b1f97d0374d7351a792b79624
dodo <eg4rang@naver.com>
2025-11-13 22:13:23 +0900

    [4차 필수] feat: DataSource 구현

---
+++ app/src/main/java/com/sopt/dive/data/datasource/remote/user/UserDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.sopt.dive.data.datasource.remote.user
+
+import com.sopt.dive.core.model.BaseResponse
+import com.sopt.dive.data.service.UserService
+import com.sopt.dive.data.service.dto.request.SignUpRequestDto
+import com.sopt.dive.data.service.dto.response.MemberResponseDto
+
+class UserDataSourceImpl(
+    private val userService: UserService,
+) : UserDataSource {
+
+    override suspend fun getUserInfo(userId: Int): BaseResponse<MemberResponseDto> =
+        userService.getUserInfo(userId)
+
+    override suspend fun signUp(body: SignUpRequestDto): BaseResponse<MemberResponseDto> =
+        userService.signUp(body)
+
+}
